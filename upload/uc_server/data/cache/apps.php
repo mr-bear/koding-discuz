133c5c585c9b260a59d89395c21da6bf0747fe9a
fei.xiong <fei.xiong@corp.elong.com>
2014-05-04 17:21:06 +0800

    first commit

---
+++ upload/uc_server/data/cache/apps.php
@@ -0,0 +1,23 @@
+<?php
+$_CACHE['apps'] = array (
+  1 => 
+  array (
+    'appid' => '1',
+    'type' => 'DISCUZX',
+    'name' => 'Discuz! Board',
+    'url' => 'http://jsxiong110.kd.io/discuz/upload',
+    'authkey' => 'gaI4UeS0PbJ4l6W45cCc6534D2z9seE0NcP1Z7Gfm6deLaib1713e0nb0bber7i1',
+    'ip' => '',
+    'viewprourl' => '',
+    'apifilename' => 'uc.php',
+    'charset' => '',
+    'dbcharset' => '',
+    'synlogin' => '1',
+    'recvnote' => '1',
+    'extra' => '',
+    'tagtemplates' => '',
+    'allowips' => '',
+  ),
+);
+
+?>
---
+++ upload/uc_server/data/config.inc.php
@@ -0,0 +1,18 @@
+<?php 
+define('UC_DBHOST', 'localhost');
+define('UC_DBUSER', 'root');
+define('UC_DBPW', 'root');
+define('UC_DBNAME', 'ultrax');
+define('UC_DBCHARSET', 'utf8');
+define('UC_DBTABLEPRE', 'pre_ucenter_');
+define('UC_COOKIEPATH', '/');
+define('UC_COOKIEDOMAIN', '');
+define('UC_DBCONNECT', 0);
+define('UC_CHARSET', 'utf-8');
+define('UC_FOUNDERPW', 'ab3bd08e901b48cc7475eead3c1f387b');
+define('UC_FOUNDERSALT', 'Taj44e');
+define('UC_KEY', '3aw47eN0TbH4L6m4gcrcG5q412U9aeu0gcj1A7Sf16meLawbA7V3h0RbcbOeZ7X1');
+define('UC_SITEID', 'raE4veq0ibE4Q6c4bcqct5x492N9Eea0Bc31E7xfR6eeYaBbG713d0kbpb8eY751');
+define('UC_MYKEY', 'Qam4Vex0mbY48614AcBcN5E4v2z9ve50FcV1E7SfZ6Ie1afb87e3P0ubNbLeZ721');
+define('UC_DEBUG', false);
+define('UC_PPP', 20);
