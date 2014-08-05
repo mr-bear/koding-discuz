a3c8ab0ea1b7e9639fd46a0a8d716c47cce9b411
unknown <xiongfei@TN-10-20140168.tuniu-inc.com>
2014-08-05 17:07:38 +0800

    userpopup first commit

---
+++ upload/source/plugin/mrbear_userpopup/main.class.php
@@ -0,0 +1,14 @@
+<?php
+if(!defined('IN_DISCUZ')) {
+	exit('Access Denied');
+}
+
+
+class plugin_mrbear_userpopup{
+
+}
+
+
+class plugin_mrbear_userpopup_forum extends plugin_mrbear_userpopup {
+	
+}
